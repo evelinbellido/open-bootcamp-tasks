ad8b50ac4cde1d43182e04231a4e8d0267381025
Evelin <bellidoevelin@gmail.com>
2023-01-28 18:18:23 +0000

    task numbers

---
+++ numbers.js
@@ -0,0 +1,16 @@
+//altura en centímetros
+let centimeters = 150;
+console.log(centimeters);
+//altura en metros 
+let meters = 1.55;
+console.log(meters);
+//peso en kilogramos
+let kilograms = 55.3;
+console.log(kilograms);
+//metros redondeada hacia arriba
+console.log(Math.ceil(meters));
+//kilogramos redondeado hacia abajo
+console.log(Math.floor(kilograms));
+//el máximo valor que se puede obtener en Javascript + 1
+let maxNumber = Number.MAX_VALUE ;
+console.log(maxNumber+ 1);
